cd6498077b8dca7e54571c6966d274e4475c8ac5
Ilya Kislenko <62322166+cashcats69@users.noreply.github.com>
2022-09-13 15:59:55 +0700

    feat:added css/sass loaders and css-modules

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Counter from "./components/counter";
+import './index.scss'
+const App = () => {
+	return (
+		<div className="app">
+            <h1>Css-modules</h1>
+			<Counter />
+		</div>
+	);
+};
+
+export default App;
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-import { someFn } from "./test";
-someFn(33);
-document.body.innerHTML = `<div>hello</div>`
