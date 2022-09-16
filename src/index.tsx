559e6353d7dd23feb34ea6310a1adc97c6e08035
Ilya Kislenko <62322166+cashcats69@users.noreply.github.com>
2022-09-16 11:47:58 +0700

    refactor:Absolute imports and  FSD architecture added.

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
+import App from "./app/App";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import ThemeProvider from "./styles/theme/ThemeProvider";
+import ThemeProvider from "./app/providers/ThemeProvider/ui/ThemeProvider";
 render(
 	<BrowserRouter>
 		<ThemeProvider>
