5abaab9daaef316e5ee9cec8985b2275bba073a4
Ilya Kislenko <62322166+cashcats69@users.noreply.github.com>
2022-09-27 16:33:22 +0700

    improvement: i18n babel extract plugin added

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -6,6 +6,25 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
 		test: /\.svg$/,
 		use: ["@svgr/webpack"],
 	};
+	const babelLoader = {
+		test: /\.(js|jsx|tsx)$/,
+		exclude: /node_modules/,
+		use: {
+			loader: "babel-loader",
+			options: {
+				presets: ["@babel/preset-env"],
+				plugins: [
+					[
+						"i18next-extract",
+						{
+							locales: ["ru", "en"],
+							keyAsDefaultValue: true,
+						},
+					],
+				],
+			},
+		},
+	};
 	const cssLoader = {
 		test: /\.s[ac]ss$/i,
 		use: [
@@ -38,5 +57,5 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
 		],
 	};
 
-	return [fileLoader, svgLoader, typescriptLoader, cssLoader];
+	return [fileLoader, svgLoader, babelLoader, typescriptLoader, cssLoader];
 }
