61ec41cedaa537bb78396335f3b5f6d931a2d430
Ilya Kislenko <62322166+cashcats69@users.noreply.github.com>
2022-10-02 16:24:31 +0700

    test: added jest test to classNames helper

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,12 @@ module.exports = {
     env: {
         browser: true,
         es2021: true,
+        jest: true,
     },
     extends: [
         'plugin:react/recommended',
         'airbnb',
-        'plugin:i18next/recommended'
+        'plugin:i18next/recommended',
     ],
     parser: '@typescript-eslint/parser',
     parserOptions: {
@@ -38,6 +39,7 @@ module.exports = {
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
         'i18next/no-literal-string': ['error', { markupOnly: true }],
+        'max-len': ['error', { code: 90, ignoreComments: true }],
     },
     globals: {
         __IS_DEV__: true,
---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,24 @@
+import { text } from 'stream/consumers';
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+    test('Only first parameter', () => {
+        expect(classNames('someClass')).toBe('someClass');
+    });
+    test('With additional class', () => {
+        const expected = 'someClass somethingNew1 somethingNew2';
+        expect(classNames('someClass', {}, ['somethingNew1', 'somethingNew2'])).toBe(expected);
+    });
+    test('With additional classes and mods', () => {
+        const expected = 'someClass somethingNew1 somethingNew2 hovered scrollable';
+        expect(classNames('someClass', { hovered: true, scrollable: true }, ['somethingNew1', 'somethingNew2'])).toBe(expected);
+    });
+    test('With additional classes and one of mods false', () => {
+        const expected = 'someClass somethingNew1 somethingNew2 hovered';
+        expect(classNames('someClass', { hovered: true, scrollable: false }, ['somethingNew1', 'somethingNew2'])).toBe(expected);
+    });
+    test('With additional classes and one of mods undefined', () => {
+        const expected = 'someClass somethingNew1 somethingNew2 hovered';
+        expect(classNames('someClass', { hovered: true, scrollable: undefined }, ['somethingNew1', 'somethingNew2'])).toBe(expected);
+    });
+});
