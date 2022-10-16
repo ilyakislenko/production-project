50730614987c174ba2fbc0fad831d8768dd34cc3
Ilya Kislenko <62322166+cashcats69@users.noreply.github.com>
2022-10-16 13:57:44 +0700

    improvement: upgrade sidebar, add button themes

--- src/shared/ui/AppLink/AppLink.tsx
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -26,5 +26,3 @@ export const AppLink: FC<AppLinkProps> = (props) => {
         </Link>
     );
 };
-
-export default AppLink;
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,7 +2,7 @@ import { Theme, useTheme } from 'app/providers/ThemeProvider';
 import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
-import { Button, ThemeButton } from 'shared/ui/Button/Button';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import DarkIcon from 'shared/assets/icons/search-svg-dark.svg';
 import LightIcon from 'shared/assets/icons/search-svg-light.svg';
 import NotificationDark from 'shared/assets/icons/notification-dark.svg';
@@ -19,18 +19,17 @@ export const Navbar = ({ className }: NavbarProps) => {
     return (
         <div className={classNames(cls.navbar, {}, [className])}>
             <div className={cls.links}>
-                <Button theme={ThemeButton.CLEAR} className={classNames(cls.search, {}, [className])}>
+                <Button theme={ButtonTheme.CLEAR} className={classNames(cls.search, {}, [className])}>
                     {
                         theme === Theme.DARK ? <LightIcon /> : <DarkIcon />
                     }
                 </Button>
-                <Button theme={ThemeButton.CLEAR} className={classNames(cls.notification, {}, [className])}>
+                <Button theme={ButtonTheme.CLEAR} className={classNames(cls.notification, {}, [className])}>
                     {
                         theme === Theme.DARK ? <NotificationLight /> : <NotificationDark />
                     }
                 </Button>
-                <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={cls.mainLink}>{t('Главная')}</AppLink>
-                <AppLink theme={AppLinkTheme.SECONDARY} to="/about">{t('О сайте')}</AppLink>
+
             </div>
         </div>
     );
