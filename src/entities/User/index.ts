d40aa0d21c6883c8525d4527803dbd32dba2dd2b
ilya kislenko <ikis.dev01@gmail.com>
2023-11-17 08:48:01 +0700

    feat: new pages

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -1,3 +1,4 @@
 export { userReducer, userActions } from './model/slice/userSlice';
 export { UserSchema, User } from './model/types/user';
 export { getUserAuthData } from '../User/model/selectors/getUserAuthData/getUserAuthData';
+export { getUserInited } from '../User/model/selectors/getUserInited/getUserInited';
